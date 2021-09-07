2cc5fb6319824cd4b48386f3b3acdd565485ec32
Carah29 <48199500+Carah29@users.noreply.github.com>
2021-09-07 19:30:03 +0000

    Update emailslicer.py

--- emailslicer.py
+++ emailslicer.py
@@ -1,8 +1,16 @@
-email = input("Enter email: ")
+#trying to check if its a valid email
+#need to do a better verification
+#mayb using regex
+
+while True:
+    email = input("Enter email: ")
+    if email.find("@") == -1:
+        continue
+    else:
+        break
 email_slicer = email.split(sep="@")
 
 username = email_slicer[0]
-
-#domain name
 domain_name = email_slicer[1]
+
 print(username, domain_name)
